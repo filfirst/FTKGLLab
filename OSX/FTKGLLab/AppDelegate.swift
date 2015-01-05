a5c6e74060f0e1645d84b07db7348026fd44ec69
Alex Wang <filfirst@gmail.com>
2015-01-05 22:57:53 +0800

    Initial commit

---
+++ OSX/FTKGLLab/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  FTKGLLab
+//
+//  Created by Alex Wang on 1/5/15.
+//  Copyright (c) 2015 ftk. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
